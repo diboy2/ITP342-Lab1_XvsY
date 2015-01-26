84ea27244f7b12050cffe0dd8e7a33ad5a7344d3
ITP Student <ITP@guest-wireless-207-151-035-046.usc.edu>
2015-01-26 13:08:32 -0800

    Added message text. Need to align.

--- Lab1/ViewController.m
+++ Lab1/ViewController.m
@@ -17,10 +17,24 @@
 
 @implementation ViewController
 - (IBAction)ninja_button:(id)sender {
-    self.message.text = @ " I'm a ninja";
+    NSString *str1 = @"Congratulations, ";
+    NSString *str2 = @". \n You are now a ninja. ";
+    str1 = [str1 stringByAppendingString:@"Bob"];
+    str1 = [str1 stringByAppendingString:str2];
+    str1 = [str1 stringByAppendingString:@" \n You are a mercenary of feudal Japan. "];
+    str1 = [str1 stringByAppendingString:@"\nYour tasks include espionage, sabotage, infiltration, and assassination, and open combat in certain situations. "];
+    self.message.text = str1;
+    self.message.textAlignment = NSTextAlignmentCenter;
 }
 - (IBAction)pirate_button:(id)sender {
-    self.message.text = @ " I'm a pirate";
+    NSString *str1 = @"Contratulations, ";
+    NSString *str2 = @". \n You are now a pirate.";
+    str1 = [str1 stringByAppendingString:@"Joe."];
+    str1 = [str1 stringByAppendingString:str2];
+    str1 = [str1 stringByAppendingString:@" \nYou are a scourge of the seven seas."];
+    str1 = [str1 stringByAppendingString:@"\nYour tasks include plundering, firing cannons, and committing illegal acts at sea."];
+    
+    self.message.text = str1;
     
 }
 - (IBAction)background_button:(id)sender {
